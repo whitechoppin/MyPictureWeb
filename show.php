af67048854238b227de6018b34de6c07c54ace40
Alex Pratama Wibowo <alexanderwibowo.aw@gmail.com>
2019-06-22 23:53:00 +0800

    gMBAR

--- show.php
+++ show.php
@@ -2,29 +2,58 @@
 <html>
 <head>
     <title>Show Data With Description</title>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="js/azurescript.js"></script>
 </head>
-<body>
-    <h1>Analyze image:</h1>
-    Enter the URL to an image, then click the <strong>Analyze image</strong> button.
-    <br><br>
-    Image to analyze:
-    <input type="text" name="inputImage" id="inputImage"
-        value="http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
-    <button onclick="processImage()">Analyze image</button>
-    <br><br>
-    <div id="wrapper" style="width:1020px; display:table;">
-        <div id="jsonOutput" style="width:600px; display:table-cell;">
-            Response:
-            <br><br>
-            <textarea id="responseTextArea" class="UIInput"
-                    style="width:580px; height:400px;"></textarea>
+<body onload="processImage()">
+    <div class="container">
+        <div class="row">
+            <h3>Lihat Gambar</h3>
         </div>
-        <div id="imageDiv" style="width:420px; display:table-cell;">
-            Source image:
-            <br><br>
-            <img id="sourceImage" width="400" />
+        <?php
+            $serverName = "tcp:mypictureweb.database.windows.net,1433";
+            $connectionOptions = array(
+                "Database" => "mypictureweb", // update me
+                "Uid" => "alexwibowo", // update me
+                "PWD" => "08Maret2017" // update me
+            ); 
+            $conn = sqlsrv_connect($serverName, $connectionOptions);  
+            
+            if ($conn === false)  
+            {  
+                die(print_r(sqlsrv_errors() , true));  
+            }  
+            $id = null;
+            if ( !empty($_GET['id'])) {
+                $id = $_REQUEST['id'];
+            }
+            
+            $sql = "SELECT * FROM gambar where id = ?";
+            $params = array($id);  
+            $getResults = sqlsrv_query($conn, $sql, $params); 
+            $data = sqlsrv_fetch_array($getResults);
+            $conn = null;  
+        ?>
+        <div class="row">
+            <input type="text" name="inputImage" id="inputImage"
+                value= "https://mypictureweb.blob.core.windows.net/<?php echo $data['alamat'];?>" />
+            <button>Analyze image</button>
+        </div>
+        <div id="wrapper" style="width:1020px; display:table;">
+            <div id="jsonOutput" style="width:600px; display:table-cell;">
+                Response:
+                <br><br>
+                <textarea id="responseTextArea" class="UIInput"
+                        style="width:580px; height:400px;"></textarea>
+            </div>
+            <div id="imageDiv" style="width:420px; display:table-cell;">
+                Source image:
+                <br><br>
+                <img id="sourceImage" width="400" />
+            </div>
         </div>
     </div>
 </body>
