e6c29c21e1e805944de87b3dbd78b16b18807943
Alex Pratama Wibowo <alexanderwibowo.aw@gmail.com>
2019-06-23 01:12:18 +0800

    json

--- js/azurescript.js
+++ js/azurescript.js
@@ -1,23 +1,8 @@
 
 function processImage() {
-    // **********************************************
-    // *** Update or verify the following values. ***
-    // **********************************************
-
-    // Replace <Subscription Key> with your valid subscription key.
     var subscriptionKey = "184c9d6a07f5427db4111ded93de9f9c";
-
-    // You must use the same Azure region in your REST API method as you used to
-    // get your subscription keys. For example, if you got your subscription keys
-    // from the West US region, replace "westcentralus" in the URL
-    // below with "westus".
-    //
-    // Free trial subscription keys are generated in the "westus" region.
-    // If you use a free trial subscription key, you shouldn't need to change
-    // this region.
     var uriBase =
         "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
-
     // Request parameters.
     var params = {
         "visualFeatures": "Categories,Description,Color",
@@ -49,8 +34,8 @@ function processImage() {
     .done(function(data) {
         // // Show formatted JSON on webpage.
         $("#responseTextArea").val(JSON.stringify(data, null, 2));
-        obj = JSON.parse(data);
-        alert(obj.captions[0].text);
+        // obj = JSON.parse(data);
+        alert(data.captions[0].text);
         
     })
 
